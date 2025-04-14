7c63173b761fa89be5e148c89a4dd2c849595fa7
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2025-04-14 15:38:18 +0530

    feat: Refactor JViewer component to implement tabbed interface and improve JSON rendering

--- frontend/src/components/JViewer/JViewer.jsx
+++ frontend/src/components/JViewer/JViewer.jsx
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactJson from "react-json-view";
+import {
+  Tabs,
+  Tab,
+  Dropdown,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@carbon/react";
+
+const feedbackDates = ["2025-04-01", "2025-03-28", "2025-03-15", "2025-02-20"];
+
+const jsonViewFucntion = (data) => {
+  return (
+    <div
+      style={{
+        overflowY: "auto",
+        maxHeight: "80dvh",
+        fontSize: "var(--cds-body-compact-01-font-size, .875rem)",
+        fontWeight: "var(--cds-body-compact-01-font-weight, 400)",
+        lineHeight: " var(--cds-body-compact-01-line-height, 1.28572)",
+      }}
+    >
+      <ReactJson
+        src={data}
+        name={false}
+        collapsed={2}
+        enableClipboard={false}
+        displayDataTypes={false}
+        style={{ fontSize: "14px" }}
+      />
+    </div>
+  );
+};
 
 const JViewer = ({ data }) => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
   return (
     <div style={{ margin: "1rem" }}>
-      <h4 style={{ marginBottom: "1rem" }}> JSON View</h4>
-      <div
-        style={{
-          overflowY: "auto",
-          maxHeight: "80dvh",
-          fontSize: "var(--cds-body-compact-01-font-size, .875rem)",
-          fontWeight: "var(--cds-body-compact-01-font-weight, 400)",
-          lineHeight: " var(--cds-body-compact-01-line-height, 1.28572)",
-        }}
-      >
-        <ReactJson
-          src={data}
-          name={false}
-          collapsed={2}
-          enableClipboard={false}
-          displayDataTypes={false}
-          style={{ fontSize: "14px" }}
-        />
-      </div>
+      <Tabs>
+        <TabList>
+          <Tab>Json View</Tab>
+          <Tab>Feedback History</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>{jsonViewFucntion(data)}</TabPanel>
+          <TabPanel>Tab Panel 2</TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 };
